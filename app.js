380396f02e40281aa1f731127f7ccb7b2d180090
baljinder-singh <baljinder.singh@comprotechnologies.com>
2024-04-06 17:18:33 +0530

    code updated

--- app.js
+++ app.js
@@ -5,14 +5,40 @@ var redis = require('redis');
 
 const app = express();
 
+var client = redis.createClient('12611', 'redis-12611.c1.us-west-2-2.ec2.cloud.redislabs.com');
+
+client.auth('qWj1TjnQ649eEHajzsfFmYO3yIIDDDIS', function (err) {
+  if (err) {
+    console.log('err');
+  }
+});
+
+client.on('connect', function () {
+  console.log("connected");
+});
+
+client.on('error', function (error) {
+  console.log("error", error);
+});
+
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 app.use(cors()); // Enable CORS for all routes
 
 // Routes
-app.get('/', (req, res) => {
-  client = cacheClient();
+app.get('/setData', (req, res) => {
+  console.log('req.query');
+  console.log(req.query);
+  setDataInCache();
+  res.send('Hello World Duniya!');
+
+});
+
+app.get('/getData', (req, res) => {
+  console.log('req.query');
+  console.log(req.query);
+  getDataFromCache();
   res.send('Hello World Duniya!');
 
 });
@@ -24,33 +50,16 @@ app.listen(PORT, () => {
 });
 
 
-function cacheClient() {
-  var client = redis.createClient('12611', 'redis-12611.c1.us-west-2-2.ec2.cloud.redislabs.com');
-
-  client.auth('qWj1TjnQ649eEHajzsfFmYO3yIIDDDIS', function(err) {
-    if(err) {
-      console.log('err');
-    }
-  });
-
-  client.on('connect', function () {
-    console.log("connected");
-  });
-
-  client.on('error', function (error) {
-    console.log("error", error);
-  });
-
-
+function setDataInCache() {
   client.set('rishav', 'Navin', function (err, res) {
     console.log(res);
     console.log(err);
   });
+}
 
+function getDataFromCache(client) {
   client.get('rishav', function (err, res) {
     console.log(res);
     console.log(err);
   });
-
-  return client;
 }
