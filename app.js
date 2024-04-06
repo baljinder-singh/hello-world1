1e73b85307e99b90f19c3bd5d35dd980287b023f
baljinder-singh <baljinder.singh@comprotechnologies.com>
2024-04-06 10:00:08 +0530

    redis added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // app.js
 const express = require('express');
 const cors = require('cors');
+var redis = require("redis");
 
 const app = express();
+
+cacheClient();
+
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
@@ -10,11 +14,37 @@ app.use(cors()); // Enable CORS for all routes
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
+
+function cacheClient() {
+  var client = redis.createClient('6379', 'redis://red-co8cu4gl5elc738sqsf0');
+
+  client.on('connect', function () {
+    console.log("connected");
+  });
+
+  client.on('error', function (error) {
+    console.log("error", error);
+  });
+
+
+  client.set('rishav', 'Navin', function (err, res) {
+    console.log(res);
+    console.log(err);
+  });
+
+  client.get('rishav', function (err, res) {
+    console.log(res);
+    console.log(err);
+  });
+
+  return client;
+}
