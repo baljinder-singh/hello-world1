31710cd5651145466fac69e6a9d3743f91d12016
baljinder-singh <baljinder.singh@comprotechnologies.com>
2024-04-06 17:41:41 +0530

    code modified

--- app.js
+++ app.js
@@ -26,21 +26,32 @@ client.on('error', function (error) {
 app.use(express.json()); // Parse JSON bodies
 app.use(cors()); // Enable CORS for all routes
 
-// Routes
+// POST route handler
 app.post('/', (req, res) => {
-  console.log('req.query');
-  console.log(req.query);
-  client.set('rishav', 'Navin', function (err, output) {
-    console.log(output);
-    console.log(err);
-    res.send('Hello World Duniya!', output);
-  });
+    // Log request query parameters
+    console.log('Request Query Parameters:');
+    console.log(req.query);
 
+    // Log request body (parsed by bodyParser middleware)
+    console.log('Request Body:');
+    console.log(req.body);
 
+    // Example: Set a value in Redis
+    const key = 'rishav';
+    const value = 'Navin';
+
+    client.set(key, value, (err, reply) => {
+        if (err) {
+            console.error('Redis set error:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        console.log('Redis set response:', reply);
+        res.send('Hello World Duniya!'); // Send response to client
+    });
 });
 
 app.get('/', (req, res) => {
-  console.log('req.query');
+  console.log('req.query in get');
   console.log(req.query);
   client.get('rishav', function (err, output) {
     console.log(output);
